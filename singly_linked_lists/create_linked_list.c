053a5559e3b75c2f875b6c18dd01c05991e3aaff
felabel <felicityabel2016@gmail.com>
2024-01-21 16:24:10 +0100

    traversing and printing data in a linked list

--- singly_linked_lists/create_linked_list.c
+++ singly_linked_lists/create_linked_list.c
@@ -7,6 +7,23 @@ struct node{
     struct node *link;
 };
 
+// Function to count the number of nodes in the linked list
+void count_nodes(struct node *head) {
+    int count = 0;
+    if (head == NULL)
+        printf("LINKED LIST IS EMPTY");
+    struct node *ptr = NULL;
+    ptr = head;
+    while (ptr != NULL) {
+        count++;
+        printf("%d", ptr->data);
+        ptr = ptr->link;
+
+    }
+    printf("%d", count);
+}
+
+
 int main() {
     struct node *head = malloc(sizeof(struct node));
     head->data = 50;
@@ -18,14 +35,16 @@ int main() {
     current->link = NULL;
     // update the link of the head to point to the address of the second node
     head->link = current;
-
     // creating the third node
     current = malloc(sizeof(struct node));
     current->data = 3;
     current->link = NULL;
 
     head->link->link = current;
+    // Call the count_nodes function to print the count
+    count_nodes(head);
     return 0;
+
 }
 
 
